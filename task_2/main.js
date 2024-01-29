effb12cccd4173edadf7359bb469b8c33ce37b9c
Niwoone <artemsnejok222@gmail.ru>
2024-01-29 18:55:35 +0300

    Написал код на основе if....else

--- task_2/main.js
+++ task_2/main.js
@@ -1,9 +1,9 @@
-const temperature = prompt("Введите значение температуры");
+let temperature = prompt("Введите температуру:");
 
-if ( temperature >= 30 ) {
-    console.log("Можете надеть лёгкую одежду");
-} else if ( temperature >= 20 ) {
-    console.log("Одеть тёплую одежду");
-} else if (temperature <= 10 ) {
-    console.log("Одеть зимнюю одежду");
+if (temperature > 30) {
+    console.log("Вы можете надеть легкую одежду.");
+} else if (temperature > 20) {
+    console.log("Нужно надеть теплую одежду.");
+} else if (temperature <= 20) {
+    console.log("Сегодня холодно и нужно надевать зимнюю одежду.");
 }
