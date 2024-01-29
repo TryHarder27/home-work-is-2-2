858d7f9770a51915cdc21776e3ac79d1b86c5d1b
Niwoone <artemsnejok222@gmail.ru>
2024-01-29 18:59:47 +0300

    Написал многомерный массив

--- task_2/main.js
+++ task_2/main.js
@@ -1,9 +1,2 @@
-let temperature = prompt("Введите температуру:");
-
-if (temperature > 30) {
-    console.log("Вы можете надеть легкую одежду.");
-} else if (temperature > 20) {
-    console.log("Нужно надеть теплую одежду.");
-} else if (temperature <= 20) {
-    console.log("Сегодня холодно и нужно надевать зимнюю одежду.");
-}
+let studentsandteachers = [["Александр Рыло", "Вадим Клочков", "Йосиф Руденко"],["Эльдар Комаров", "Татьяна Короткова"]]
+console.log(studentsandteachers)
