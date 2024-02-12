6774207b084c7ced1b631e86632ae50af493adb0
Niwoone <artemsnejok222@gmail.ru>
2024-02-12 16:52:29 +0300

    Создали массив объектов и вывели значения определенного свойства для каждого объекта

--- tusk_4/app.js
+++ tusk_4/app.js
@@ -1,26 +1,18 @@
-let array = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
-console.log("Двумерный массив:");
-for (let i of array)
-{
-        for (let item of i)
-        {
-            console.log(item);
-        }
-    }
+ let users = [{ name: "Dima", age: 25}, { name: "Katy", age: 19}, { name: "Artem", age: 35},];
+ console.log("Перед изменением:");
 
-    let sum = 0;
-    for (let a of array)
-    {
-        for (let item of a)
-        {
-            sum += item;
-        }
-    }
-    console.log("Сумма всех элементов:", sum);
+ for (let user of users)
+ {
+    console.log(user.name);
+ }
 
+ for (let user of users)
+ {
+    user.age +=1;
+ }
+ console.log("После изменения:");
 
-    console.log("Количество элементов в каждом вложенном массиве:");
-    for (let b of array)
-    {
-        console.log(b.length);
-    }   
+ for (let user of users)
+ {
+    console.log(`${user.name} теперь ${user.age} лет`);
+ }
