888a54dd271dfe964f0eca45dcf0d1cfaa9a0527
Niwoone <artemsnejok222@gmail.ru>
2024-02-26 14:01:08 +0300

    Написал функцию, которая принимает на вход два числа и возвращает их разность

---
+++ Практическая работа 2.0/app.js
@@ -0,0 +1,8 @@
+function calculateDifference(a, b) {
+    return (a - b);
+}
+
+const num1 = 54;
+const num2 = 46;
+const difference = calculateDifference (num1, num2);
+console.log(`Разница между первым и вторым числом: ${num1} - ${num2} ответ: ${difference}.`);
